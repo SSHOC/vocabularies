e364fc3817279100e0da2a59bbb57a7fd9e584a4
Klaus Illmayer <klaus.illmayer@oeaw.ac.at>
2021-08-25 13:04:53 +0200

    Putting for eosc-life-cycle-status the description of terms into skos:definition

--- eosc/life-cycle-status/eosc-life-cycle-status.py
+++ eosc/life-cycle-status/eosc-life-cycle-status.py
@@ -2,11 +2,8 @@ import sys
 sys.path.append("..")
 from eoscskos import EoscSkos
 
-translate_table = {ord('/'): 'SLASH', ord('+'): 'PLUS', ord('('): 'OPEN_PARENTHESIS', ord(')'): 'CLOSE_PARENTHESIS',
-                   ord('&'): 'AMPERSAND', ord(','): 'COMMA'}
-
 def createSkos():
-    eosc = EoscSkos("LIFE_CYCLE_STATUS", "eosc-life-cycle-status", "EOSC Life Cycle Status List", "EOSC Life Cycle Status List from https://api.eosc-portal.eu/vocabulary/byType/LIFE_CYCLE_STATUS")
+    eosc = EoscSkos("LIFE_CYCLE_STATUS", "eosc-life-cycle-status", "EOSC Life Cycle Status List", "EOSC Life Cycle Status List from https://api.eosc-portal.eu/vocabulary/byType/LIFE_CYCLE_STATUS", "skos:definition")
     eosc.createSkos()
 
 createSkos()
